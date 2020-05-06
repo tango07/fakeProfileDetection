488129e2ed48cff177906efe115fca49a48c0e46
akshay hariwal <akshaykumar.hariwal@hashedin.com>
2020-05-06 20:52:17 +0530

    done with extension version 1

--- mlfake/app/routes.py
+++ mlfake/app/routes.py
@@ -4,43 +4,67 @@ from app.model import Predict
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        data= request.form.to_dict()
+        int_feature = list(data.keys())
+        features_values =list(data.values())
+        int_features_values = [int(data) for data in features_values]
+        list_int_features_values =[int_features_values]
+        output = Predict().prediction(list_int_features_values)
+        # print(int_features_values)
+        # statuses = int(request.form["statuses"])
+        # friends = int(request.form["friends"])
+        # favourites = int(request.form["favourites"])
+        # listed = int(request.form["listed"])
+        # statuses = int(request.form["statuses"])
+        # sex = int(request.form["sex"])
+        # lang = int(request.form["lang"])
+        # name = request.form["foo"]
+        # lorem = request.form["lorem"]
+        print(output)
+        return output
+    return "some error occured!!!"
 
 
-@app.route('/hello-world', methods=['PUT', 'GET', 'POST', 'DELETE'])
-#     sex_code_dict = {'female': -2, 'mostly_female': -1, 'unknown': 0, 'andy': 0, 'mostly_male': 1, 'male': 2}
-
-def predict():
-    int_feature = []
-    for x in request.form.values():
-         if x.isdigit():
-             int_feature.append(int(x))
-         else:
-             if x=="Male":
-                 x=2
-             elif x=="Female":
-                 x=-2
-             elif x=="Mostly Male":
-                 x=1
-             elif x=="Mostly Female":
-                 x=-1
-             elif x=="Unknown":
-                 x=0
-             int_feature.append(int(x))
-    list_int_features = [int_feature]
-    output = ""
-    if request.method == 'POST':
-    #     data = request.json
-    #     l = [[data['statuses_count'], data['followers_count'], data['friends_count'], data['favourites_count'],
-    #           data['listed_count'], data['sex_code'], data['lang_code']]]
-        print(int_feature)
-        output = Predict().prediction(list_int_features)
-        color="green"
-        if output == "FAKE":
-            color = "red"
-    return render_template('result.html', prediction_text=output, result_color=color)
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+#
+#
+# @app.route('/hello-world', methods=['PUT', 'GET', 'POST', 'DELETE'])
+# #     sex_code_dict = {'female': -2, 'mostly_female': -1, 'unknown': 0, 'andy': 0, 'mostly_male': 1, 'male': 2}
+#
+# def predict():
+#     int_feature = []
+#     for x in request.form.values():
+#          if x.isdigit():
+#              int_feature.append(int(x))
+#          else:
+#              if x=="Male":
+#                  x=2
+#              elif x=="Female":
+#                  x=-2
+#              elif x=="Mostly Male":
+#                  x=1
+#              elif x=="Mostly Female":
+#                  x=-1
+#              elif x=="Unknown":
+#                  x=0
+#              int_feature.append(int(x))
+#     list_int_features = [int_feature]
+#     output = ""
+#     if request.method == 'POST':
+#     #     data = request.json
+#     #     l = [[data['statuses_count'], data['followers_count'], data['friends_count'], data['favourites_count'],
+#     #           data['listed_count'], data['sex_code'], data['lang_code']]]
+#         print(int_feature)
+#         output = Predict().prediction(list_int_features)
+#         color="green"
+#         if output == "FAKE":
+#             color = "red"
+#     return render_template('result.html', prediction_text=output, result_color=color)
 
 
 if __name__ == "__main__":
--- mlfake/chrome-ext-sample-1.0.0/scripts/extension.js
+++ mlfake/chrome-ext-sample-1.0.0/scripts/extension.js
@@ -1,29 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("status").textContent = "Extension loaded";
-  var button = document.getElementById("changelinks");
+//  document.getElementById("status").textContent = "Extension loaded";
+  var button = document.getElementById("btn-post");
   button.addEventListener("click", function () {
-    $("#status").html("Clicked change links button");
-    let text = [];
-    text[0] = $("#text0").val();
-    text[1] = $("#text1").val();
-    text[2] = $("#text2").val();
-    text[3] = $("#text3").val();
-    text[4] = $("#text4").val();
-    text[5] = $("#text5").val();
-    text[6] = $("#text6").val();
-    window.onload = function () {
-      document.getElementById("submitbutton").click();
-    };
-
-    // if (!text) {
-    //   $("#status").html("Invalid text provided");
-    //   return;
-    // }
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { data: text }, function (response) {
-        $("#status").html("changed data in page");
-        console.log("success");
-      });
-    });
+        var req = new XMLHttpRequest();
+        var result = document.getElementById('result');
+        req.onreadystatechange = function()
+        {
+          if(this.readyState == 4 && this.status == 200) {
+            result.innerHTML = this.responseText;
+            alert(this.responseText)
+          } else {
+            result.innerHTML = "error";
+          }
+        }
+        req.open('POST', 'http://127.0.0.1:5000/', true);
+        req.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        req.send("statuses="+document.getElementById('text0').value+"&followers="+document.getElementById('text1').value
+        +"&friends="+document.getElementById('text2').value+"&favourites="+document.getElementById('text3').value
+        +"&listed="+document.getElementById('text4').value+"&sex="+document.getElementById('text5').value
+        +"&lang="+document.getElementById('text6').value);
+//    $("#status").html("Clicked change links button");
+//    let text = [];
+//    text[0] = $("#text0").val();
+//    text[1] = $("#text1").val();
+//    text[2] = $("#text2").val();
+//    text[3] = $("#text3").val();
+//    text[4] = $("#text4").val();
+//    text[5] = $("#text5").val();
+//    text[6] = $("#text6").val();
+//    window.onload = function () {
+//      document.getElementById("submitbutton").click();
+//    };
+//
+//    // if (!text) {
+//    //   $("#status").html("Invalid text provided");
+//    //   return;
+//    // }
+////    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+////      chrome.tabs.sendMessage(tabs[0].id, { data: text }, function (response) {
+////        $("#status").html("changed data in page");
+////        console.log("success");
+////      });
+////    });
+//        chrome.tabs.query({
+//        active: true,
+//        lastFocusedWindow: true
+//        }, function(tabs) {
+//        var tab=tabs[0];
+//            console.log(tab.url);
+//        var xhr = new XMLHttpRequest();
+//
+//            xhr.addEventListener("readystatechange", function () {
+//            if (xhr.readyState == 4) {
+//                console.log(xhr.responseText);
+//                alert(xhr.responseText)
+//          }
+//        });
+//         formData = {
+//        "data[tumblelog]": "drunknight",
+//        "data[source]": "FOLLOW_SOURCE_REBLOG"
+//    };
+//            xhr.open("POST", "http://localhost:5000/",true);
+//            xhr.send(JSON.stringify(formData));
+//        });
   });
 });
